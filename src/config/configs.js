7f9a01853aa40d8dd2b1e200f33b3c4068bc2b5a
lhmoreno <lhmorenoti@hotmail.com>
2020-07-25 22:33:21 -0300

    add configs idioma and tema

---
+++ src/config/configs.js
@@ -0,0 +1,28 @@
+const Configs = {
+    configPt: {
+        tema: 'Tema',
+        temaColor: ['Claro', 'Escuro'],
+        idioma: 'Idioma - Português',
+        entrada: 'Entrada',
+        chave: 'Chave',
+        encriptar: 'Encriptar',
+        desencriptar: 'Desencriptar',
+        saida: 'Saída',
+        alert: 'Entrada ou chave vazia!'
+    },
+    configEn: {
+        tema: 'Theme',
+        temaColor: ['Light', 'Dark'],
+        idioma: 'Language - English',
+        entrada: 'Input',
+        chave: 'Key',
+        encriptar: 'Encrypt',
+        desencriptar: 'Decrypt',
+        saida: 'Output',
+        alert: 'Input or key empty!'
+    },
+    configClaro: '--background-color: #FFFFFF; --text-color: #2A2C2D; --background-color-output: #F1F1F1',
+    configEscuro: '--background-color: #1F232D; --text-color: #FFFFFF; --background-color-output: #15171E77'
+}
+
+export default Configs
